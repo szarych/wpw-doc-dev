1ffa2182a3600b5198dd47a6bd027c65d78785a9
szarych <sebastian.zarych@worldpay.com>
2017-09-14 16:50:34 +0200

    update

--- travis.sh
+++ travis.sh
@@ -24,7 +24,7 @@
 set -e
 
 # Run build and terminate on error
-"$(yarn bin)"/gulp build --clean --optimize --revision
+"$(yarn bin)"/gulp build --clean --optimize --revision --no-lint
 
 # Deploy documentation to GitHub pages
 if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
